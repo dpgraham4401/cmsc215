a99342626d2c21f90581b5a45ecac3be4d9e5b67
David Graham <dpgraham4401@gmail.com>
2025-11-03 18:43:01 -0500

    add junit to maven build

---
+++ src/test/java/com/vimpirate/TestSetup.java
@@ -0,0 +1,15 @@
+package com.vimpirate;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TestSetup {
+
+    @Test
+    @DisplayName("Our testing setup is working")
+    public void testSetup() {
+        // This test should always pass
+        int result = 25;
+        assert(true);
+    }
+}
